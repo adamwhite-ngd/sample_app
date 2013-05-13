86803824060b8be0c47002e6f6f4d455a2b7a637
Adam White <awhite@nextgenerationdata.com>
2013-05-13 10:32:56 +0100

    Finish sign in

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 SampleApp::Application.routes.draw do
   # get "users/new"
   resources :users
+  resources :sessions, only: [:new, :create, :destroy]
 
   # get "static_pages/home"
   # get "static_pages/help"
@@ -8,10 +9,12 @@ SampleApp::Application.routes.draw do
   # get "static_pages/contact"
   root to: 'static_pages#home'
   
-  match '/help', to: 'static_pages#help'
-  match '/about', to: 'static_pages#about'
+  match '/help',    to: 'static_pages#help'
+  match '/about',   to: 'static_pages#about'
   match '/contact', to: 'static_pages#contact'
-  match '/signup', to: 'users#new'
+  match '/signup',  to: 'users#new'
+  match '/signin',  to: 'sessions#new'
+  match '/signout', to: 'sessions#destroy', via: :delete
 
   # The priority is based upon order of creation:
   # first created -> highest priority.
